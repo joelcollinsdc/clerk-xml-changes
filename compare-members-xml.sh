afebf2178d0e0a8447068ac0834d6426b4d75a40
Joel Collins <jconline@gmail.com>
2016-11-23 09:49:30 -0500

    updates

--- compare-members-xml.sh
+++ compare-members-xml.sh
@@ -3,7 +3,7 @@ set -e
 
 CURL=/usr/bin/curl
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
+TODAY=$(date +"%Y%m%d")
 URL=http://clerk.house.gov/xml/lists/MemberData.xml
 CACHEFLDR=$DIR/cache
 TMPFILE=$(mktemp)
@@ -13,5 +13,7 @@ LATEST=$(ls -t MemberData* | head -1)
 
 if [ $(diff $TMPFILE $LATEST | wc -l) -ne 0 ]; then
   echo "Changes found... copying"
-  cp $TMPFILE $CACHEFLDR/MemberData.$(date +"%Y%m%d").xml
+  cp $TMPFILE $CACHEFLDR/MemberData.$TODAY.xml
+else
+  echo "No changes found on $TODAY, latest is still $LATEST"
 fi
